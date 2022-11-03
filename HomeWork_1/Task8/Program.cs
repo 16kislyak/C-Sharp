aa50a74aae6d63d434560f491219ddf6a1ff7243
16kislyak <16kislyak@gmail.com>
2022-11-03 18:47:34 +0300

    adding Task 8 HomeWork

---
+++ HomeWork_1/Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 8. Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int count = 1;
+while (count <= N)
+{
+    int div = count % 2;
+    if (div == 0) 
+    {
+        Console.Write($"{count}, ");
+    }
+     count +=1;
+}
