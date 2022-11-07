75e6c7357a02c46d67fb631e21a855f9055f0800
16kislyak <16kislyak@gmail.com>
2022-11-07 16:54:17 +0300

    adding Task 13

---
+++ HomeWork_2/Task_13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру (слева направо) заданного числа
+// или сообщает, что третьей цифры нет.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int digit = num / 100;
+if (digit == 0)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    string numline = num.ToString();
+    Console.WriteLine($"Третья цифра: {numline[2]}"!); 
+}
