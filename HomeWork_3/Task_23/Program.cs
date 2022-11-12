71f5f2a37e3be2a22d3f95144dbcdf55778e3bb2
16kislyak <16kislyak@gmail.com>
2022-11-12 23:49:57 +0300

    adding Task 23 Home Work

---
+++ HomeWork_3/Task_23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+string GetCube(int N)
+{
+    string result = "";
+    for (int i=1; i<N; i++)
+    {
+        result += (i*i*i).ToString()+", ";
+    }
+    result += (N*N*N).ToString();
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+string GetCube1 = GetCube(n);
+Console.WriteLine($"{GetCube1}");
