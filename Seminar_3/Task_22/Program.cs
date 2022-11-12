3f9af66de8e8db9421541e5c7e406eaf2256015e
16kislyak <16kislyak@gmail.com>
2022-11-12 20:02:39 +0300

    adding Task 22

---
+++ Seminar_3/Task_22/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 22. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+string GetSquare(int N)
+{
+    string result = "";
+    for (int i=1; i<N; i++)
+    {
+        result += (i*i).ToString()+", ";
+    }
+    result += (N*N).ToString();
+    return result;
+}
+Console.Clear();
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+string GetSquare1 = GetSquare(n);
+Console.WriteLine($"{GetSquare1}");
