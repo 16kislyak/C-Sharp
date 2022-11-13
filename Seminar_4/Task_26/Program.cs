b05f962b3545b3086ca50c36f6ab0c0574f7f48e
16kislyak <16kislyak@gmail.com>
2022-11-13 14:58:46 +0300

    add Task 26

---
+++ Seminar_4/Task_26/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 26. Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+int GetAmount(int A)
+{
+    int count = 0;
+    while (A!= 0)
+    {
+        A = A / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int Amount = GetAmount(N);
+Console.WriteLine(Amount);
