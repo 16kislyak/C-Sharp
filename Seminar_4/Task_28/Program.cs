4fbfcb907aa9ada22e6ed22f45c2ad3689fbe1ba
16kislyak <16kislyak@gmail.com>
2022-11-13 15:12:51 +0300

    added Task 28

---
+++ Seminar_4/Task_28/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28. Напишите программу, котораяпринимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int GetMult(int N)
+{
+    int count = 1;
+    int i = 1;
+    while (i <= N)
+    {
+        count *= i;
+        i++;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.Write("Введите число: ");
+int A = int.Parse(Console.ReadLine()!);
+int Mult = GetMult(A);
+Console.WriteLine(Mult);
