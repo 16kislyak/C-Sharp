5fdd96a334b70442d1474ebda73d7780e8d66d8b
16kislyak <16kislyak@gmail.com>
2022-11-13 21:23:32 +0300

    added Task 29 Home Work

---
+++ HomeWork_4/Task_29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29. Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] FillArray()
+{
+    int[] a = new int [8];
+    Console.WriteLine("Задайте числа массива: ");
+    for (int i = 0; i < a.Length; i++)
+    {
+        a [i] = int.Parse(Console.ReadLine()!);
+    }
+    return a;
+}
+
+Console.Clear();
+int[] array = FillArray();
+Console.Write("[");
+Console.Write(String.Join(", ", array));
+Console.Write("]");
