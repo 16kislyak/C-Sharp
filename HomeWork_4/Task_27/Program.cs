03876bfd899dcbf284ea81e3264edade8300d95f
16kislyak <16kislyak@gmail.com>
2022-11-13 21:04:17 +0300

    added Task 27 Home Work

---
+++ HomeWork_4/Task_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 27. Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int GetSum (int N)
+{
+    int sum = 0;
+    while(N > 10)
+    {
+        int remain = N % 10;
+        sum += remain;
+        N = N / 10;
+    }
+    if(N < 10)
+    {
+        sum +=N;
+    }
+    return sum;
+}
+
+Console.Clear();
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+int Sum = GetSum(A);
+Console.WriteLine($"Сумма чисел -> {Sum}");
