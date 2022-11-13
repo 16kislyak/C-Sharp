409ab4f5e22da7d9d480f382757c67e9a4731f4c
16kislyak <16kislyak@gmail.com>
2022-11-13 22:21:26 +0300

    added Task *

---
+++ HomeWork_4/Task_ExtraHard/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача “со звездочкой”. Напишите функцию, которая принимает одно число - высоту елочки
+// и рисует ее в консоли звездочками.
+
+string GetSpruce(int h)
+{
+    int i = 1;
+    string Star = "*";
+    while(i <= h)
+    {
+        int centerX = (Console.WindowWidth / 2) - (Star.Length / 2);
+        Console.SetCursorPosition(centerX, i);
+        Console.WriteLine(Star);
+        Star += "**";
+        i++;
+    }
+    return Star;
+}
+
+Console.Clear();
+Console.Write("Введите высоту ёлки: ");
+int H = int.Parse(Console.ReadLine()!);
+string Spruce = GetSpruce(H);
