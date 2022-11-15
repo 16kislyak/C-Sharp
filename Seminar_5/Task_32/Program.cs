b9d01ee6ab1b76a6a1cdd3f9da22a6935cfa4b1a
16kislyak <16kislyak@gmail.com>
2022-11-15 23:58:56 +0300

    added Task 33

---
+++ Seminar_5/Task_32/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 32. Напишите программу замена элементов массива: положительные элементы замените на
+// соответствующие отрицательные, и наоборот.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue+1);
+    }
+    return res;
+}
+
+void GetOpposite(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] *= -1;
+    }
+    Console.WriteLine(String.Join(" ", array));
+}
+
+Console.Write("Введите длину массива: ");
+int length = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение: ");
+int MinVal = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение: ");
+int MaxVal = int.Parse(Console.ReadLine()!);
+int[] Array = GetArray(length, MinVal, MaxVal);
+Console.WriteLine(String.Join(" ", Array));
+GetOpposite(Array);
