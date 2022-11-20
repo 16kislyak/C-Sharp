cfec3da4b5df00568c3313989ac76a922d9dd646
16kislyak <16kislyak@gmail.com>
2022-11-20 23:06:47 +0300

    added Task 42

---
+++ Seminar_6/Task_42/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 42. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+int GetBinariNumber(int number)
+{
+    string result = "";
+    while (number > 0)
+    {
+        result = (number % 2).ToString() + result;
+        number /= 2;
+    }
+    return int.Parse(result);
+}
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(GetBinariNumber(num));
