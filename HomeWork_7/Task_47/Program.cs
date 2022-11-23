af017df5fece9b956f23b0e4956c6bbe813b3a1c
16kislyak <16kislyak@gmail.com>
2022-11-23 22:49:49 +0300

    added Task 47 HomeWork

---
+++ HomeWork_7/Task_47/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Convert.ToDouble(new Random().Next(minValue, maxValue + 1)) / 10;
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write("{0,5:F1}", inArray[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, cols, -100, 100);
+PrintArray(array);
