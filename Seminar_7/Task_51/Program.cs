bb87b8bad6b86b209a5ac5f314b0f1828ef5ccca
16kislyak <16kislyak@gmail.com>
2022-11-23 21:45:26 +0300

    added Task 51

---
+++ Seminar_7/Task_51/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 51. Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали
+// (с индексами (0,0); (1;1) и т.д.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} \t"); // \t - табуляция
+        }
+        Console.WriteLine();
+    }
+}
+
+void DiagonalSum(int[,] oldArray)
+{
+    int Sum = 0;
+    for (int i = 0; i < oldArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < oldArray.GetLength(1); j++)
+        {
+            if (i == j) Sum += oldArray[i, j];
+        }
+    }
+    Console.WriteLine($"Сумма чисел по диагонали: {Sum}");
+}
+
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int cols = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, cols, 0, 10);
+PrintArray(array);
+DiagonalSum(array);
+
