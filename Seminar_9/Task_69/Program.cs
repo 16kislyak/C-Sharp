041c9d6d01617d6ce1ccf1e6023ab562cd62977d
16kislyak <16kislyak@gmail.com>
2022-11-30 21:17:13 +0300

    added Task 69

---
+++ Seminar_9/Task_69/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 69. Напишите программу, которая на вход принимает два числа A и B, и
+// возводит число А в целую степень B с помощью рекурсии.
+
+int square(int a, int b)
+{
+    if (b == 0) return 1;
+    return square(a, b - 1) * a;
+}
+
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(square(A, B));
