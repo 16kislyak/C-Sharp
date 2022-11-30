fb7329988e9662f70529cb5aa4526efec8c08915
16kislyak <16kislyak@gmail.com>
2022-11-30 20:12:43 +0300

    added Task 63

---
+++ Seminar_9/Task_63/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 63. Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+string NaturalNumbers(int N)
+{
+    if (N == 1) return N.ToString();
+    Console.WriteLine(N);
+    return $"{NaturalNumbers(N - 1)}, {N}";
+}
+
+int[][] data = new int[][]
+{
+    new int[] {1, 5},
+    new int[] {1, 8}
+};
+foreach (int[] item in data)
+{
+    Console.WriteLine($"{item[0], 2} -> {NaturalNumbers(item[1])}");
+}
